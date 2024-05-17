07a3f6d5d99b3a580ad5525d7d947908a0271194
CrisAOH <20031091@itcelaya.edu.mx>
2024-05-17 01:35:40 -0600

    Se ha actualizado la interfaz de la pantalla de incio de sesión.

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -3,9 +3,11 @@ import 'package:ca_mobile/common/widgets/custom_scaffold.dart';
 import 'package:ca_mobile/common/widgets/options_buttons.dart';
 import 'package:ca_mobile/features/auth/screens/ressetpassword_screen.dart';
 import 'package:ca_mobile/features/auth/screens/signup_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:icons_plus/icons_plus.dart';
 import 'package:ca_mobile/common/widgets/custom_button.dart';
 import 'package:ca_mobile/features/auth/controller/auth_controller.dart';
 
@@ -18,8 +20,10 @@ class LoginScreen extends ConsumerStatefulWidget {
 }
 
 class _LoginScreenState extends ConsumerState<LoginScreen> {
+  final _formSignInKey = GlobalKey<FormState>();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
+  bool rememberPassword = false;
 
   @override
   void dispose() {
@@ -39,111 +43,270 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return CustomScaffold(
-      child: Text("Iniciar sesión"),
+      child: Column(
+        children: [
+          const Expanded(
+            flex: 1,
+            child: SizedBox(
+              height: 10,
+            ),
+          ),
+          Expanded(
+            flex: 7,
+            child: Container(
+              padding: const EdgeInsets.fromLTRB(
+                25.0,
+                50.0,
+                25.0,
+                20.0,
+              ),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(
+                    40.0,
+                  ),
+                  topRight: Radius.circular(
+                    40.0,
+                  ),
+                ),
+              ),
+              child: SingleChildScrollView(
+                child: Form(
+                  key: _formSignInKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Iniciar Sesión",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w900,
+                          color: Colors.black,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 40.0,
+                      ),
+                      TextFormField(
+                        controller: emailController,
+                        decoration: InputDecoration(
+                          label: const Text(
+                            "Correo electrónico",
+                          ),
+                          hintText: "Ingrese su correo electrónico",
+                          hintStyle: const TextStyle(
+                            color: Colors.black26,
+                          ),
+                          border: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Colors.black12,
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Colors.black12,
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 25.0,
+                      ),
+                      TextFormField(
+                        controller: passwordController,
+                        obscureText: true,
+                        obscuringCharacter: '*',
+                        decoration: InputDecoration(
+                          label: const Text(
+                            "Contraseña",
+                          ),
+                          hintText: "Ingrese su contraseña",
+                          hintStyle: const TextStyle(
+                            color: Colors.black26,
+                          ),
+                          border: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Colors.black12,
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Colors.black12,
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 25.0,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Checkbox(
+                                value: rememberPassword,
+                                onChanged: (bool? value) {
+                                  setState(() {
+                                    rememberPassword = value!;
+                                  });
+                                },
+                                activeColor: Colors.blue,
+                              ),
+                              const Text(
+                                "Recuérdame",
+                                style: TextStyle(
+                                  color: Colors.black45,
+                                ),
+                              ),
+                            ],
+                          ),
+                          GestureDetector(
+                            child: const Text(
+                              "Olvidé mi contraseña",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.blue,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 25.0,
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            ref
+                                .read(authControllerProvider)
+                                .signInWithEmail(
+                                  context,
+                                  emailController.text,
+                                  passwordController.text,
+                                )
+                                .then(
+                              (value) {
+                                if (value) {
+                                  Navigator.pushNamed(
+                                    context,
+                                    BottomNavigation.routeName,
+                                  );
+                                } else {
+                                  print("Error al iniciar sesion.");
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                      content: Text(
+                                        'No se encontró el usuario. Cree una cuenta nueva.',
+                                      ),
+                                    ),
+                                  );
+                                }
+                              },
+                            );
+                          },
+                          child: const Text(
+                            "Ingresar",
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 25.0,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            child: Divider(
+                              thickness: 0.7,
+                              color: Colors.grey.withOpacity(0.5),
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 0, horizontal: 10),
+                            child: Text(
+                              "Iniciar sesión con",
+                              style: TextStyle(
+                                color: Colors.black45,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Divider(
+                              thickness: 0.7,
+                              color: Colors.grey.withOpacity(0.5),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 25.0,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          GestureDetector(
+                            onTap: () {},
+                            child: Brand(
+                              Brands.google,
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {},
+                            child: Brand(
+                              Brands.facebook,
+                              size: 40,
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {},
+                            child: Brand(
+                              Brands.github,
+                              size: 40,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 25.0,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            "¿No tienes una cuenta?",
+                            style: TextStyle(
+                              color: Colors.black45,
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.pushNamed(
+                                context,
+                                SignupScreen.routeName,
+                              );
+                            },
+                            child: const Text(
+                              " ¡Regístrate!",
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
-    // final size = MediaQuery.of(context).size;
-    // return Scaffold(
-    //   appBar: AppBar(
-    //     title: const Text("Inicia sesión"),
-    //     elevation: 0,
-    //     backgroundColor: Colors.white,
-    //   ),
-    //   body: SingleChildScrollView(
-    //     child: Padding(
-    //       padding: const EdgeInsets.all(18.0),
-    //       child: Column(
-    //         crossAxisAlignment: CrossAxisAlignment.stretch,
-    //         children: [
-    //           const Center(
-    //             child: Text("Inicia sesión en CalenAcademic"),
-    //           ),
-    //           const SizedBox(
-    //             height: 10,
-    //           ),
-    //           TextFormField(
-    //             controller: emailController,
-    //             keyboardType: TextInputType.emailAddress,
-    //             decoration: const InputDecoration(
-    //               hintText: 'Correo Electronico',
-    //             ),
-    //           ),
-    //           const SizedBox(
-    //             height: 10,
-    //           ),
-    //           TextFormField(
-    //             controller: passwordController,
-    //             keyboardType: TextInputType.text,
-    //             obscureText: true,
-    //             decoration: const InputDecoration(
-    //               hintText: 'Contraseña',
-    //             ),
-    //           ),
-    //           const SizedBox(
-    //             height: 8.0,
-    //           ),
-    //           GestureDetector(
-    //             onTap: () {
-    //               navigateToResetPasswordScreen(context);
-    //             },
-    //             child: const Text(
-    //               "Olvidé mi contraseña",
-    //               style: TextStyle(color: Colors.blue),
-    //             ),
-    //           ),
-    //           const SizedBox(
-    //             height: 20.0,
-    //           ),
-    //           Row(
-    //             mainAxisAlignment: MainAxisAlignment.center,
-    //             children: [
-    //               const Text("¿No tienes cuenta?"),
-    //               GestureDetector(
-    //                 onTap: () {
-    //                   navigateToSignupScreen(context);
-    //                 },
-    //                 child: const Text(
-    //                   " ¡Regístrate aquí!",
-    //                   style: TextStyle(color: Colors.blue),
-    //                 ),
-    //               ),
-    //             ],
-    //           ),
-    //           SizedBox(
-    //             height: size.height * 0.5,
-    //           ),
-    //           CustomButton(
-    //             text: 'INGRESAR',
-    //             onPressed: () {
-    //               ref
-    //                   .read(authControllerProvider)
-    //                   .signInWithEmail(
-    //                     context,
-    //                     emailController.text,
-    //                     passwordController.text,
-    //                   )
-    //                   .then(
-    //                 (value) {
-    //                   if (value) {
-    //                     Navigator.pushNamed(
-    //                         context, BottomNavigation.routeName);
-    //                   } else {
-    //                     print("Error al iniciar sesion.");
-    //                     ScaffoldMessenger.of(context).showSnackBar(
-    //                       const SnackBar(
-    //                         content: Text(
-    //                           'No se encontró el usuario. Cree una cuenta nueva.',
-    //                         ),
-    //                       ),
-    //                     );
-    //                   }
-    //                 },
-    //               );
-    //             },
-    //           ),
-    //         ],
-    //       ),
-    //     ),
-    //   ),
-    // );
   }
 }
