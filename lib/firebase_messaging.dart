181ade5ed9f7053329b774d1d64f9fac88a4ff04
AGC <anthonygcv3@gmail.com>
2024-05-29 14:40:59 -0600

    Se soluciono algunas cosas de las notificaciones en segundo plano y se habilitaron las locales para en primera instancia

--- lib/firebase_messaging.dart
+++ lib/firebase_messaging.dart
@@ -1,8 +1,22 @@
+import 'dart:convert';
+
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class FirebaseCM {
   final _firebaseMessaging = FirebaseMessaging.instance;
+  void handleMessage(RemoteMessage? message) {
+    if (message == null) return;
+    print('New');
+  }
 
+  final _androidChannel = const AndroidNotificationChannel(
+    'high_importance_channel',
+    'High Importance Notifications',
+    description: 'This channel is used for important notifications',
+    importance: Importance.defaultImportance,
+  );
+  final _localNotifications = FlutterLocalNotificationsPlugin();
   Future initPushNotifications() async {
     await FirebaseMessaging.instance
         .setForegroundNotificationPresentationOptions(
@@ -10,6 +24,26 @@ class FirebaseCM {
       badge: true,
       sound: true,
     );
+    FirebaseMessaging.instance.getInitialMessage().then(handleMessage);
+    FirebaseMessaging.onMessageOpenedApp.listen(handleMessage);
+    FirebaseMessaging.onMessage.listen((message) {
+      final notification = message.notification;
+      if (notification == null) return;
+      _localNotifications.show(
+        notification.hashCode,
+        notification.title,
+        notification.body,
+        NotificationDetails(
+          android: AndroidNotificationDetails(
+            _androidChannel.id,
+            _androidChannel.name,
+            channelDescription: _androidChannel.description,
+            icon: '@drawable/ic_launcher',
+          ),
+        ),
+        payload: jsonEncode(message.toMap()),
+      );
+    });
   }
 
   Future<void> subscribeToTopic(String topic) async {
@@ -31,10 +65,29 @@ class FirebaseCM {
   //     await _firebaseMessaging.unsubscribeFromTopic(topic);
   //   }
   // }
+  Future initLocalNotifications() async {
+    const android = AndroidInitializationSettings('@drawable/ic_launcher');
+    const settings = InitializationSettings(android: android);
+
+    await _localNotifications.initialize(
+      settings,
+      onDidReceiveNotificationResponse: (NotificationResponse response) async {
+        if (response.payload != null) {
+          final message = RemoteMessage.fromMap(jsonDecode(response.payload!));
+          handleMessage(message);
+        }
+      },
+    );
+    final platform = _localNotifications.resolvePlatformSpecificImplementation<
+        AndroidFlutterLocalNotificationsPlugin>();
+    await platform?.createNotificationChannel(_androidChannel);
+  }
 
   Future<void> initNotifications() async {
     await _firebaseMessaging.requestPermission();
-    //final fCMToken = await _firebaseMessaging.getToken();
+    final fCMToken = await _firebaseMessaging.getToken();
+    print(fCMToken);
     initPushNotifications();
+    initLocalNotifications();
   }
 }
